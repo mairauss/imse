6fb076f23449803025e226a200d1caccb2761ee1
Gerhard Nägele <a0752944@unet.univie.ac.at>
2018-01-18 14:57:10 +0100

    PHP Composer

--- htdocs/phpmongo/index.php
+++ htdocs/phpmongo/index.php
@@ -1,48 +1,6 @@
 <?php
-/*
-Quellen: 
-https://www.formget.com/login-form-in-php/
-http://www.genecasanova.com/labs/memberships/form-sessions-php.html
-*/
-session_start();
-// Variable To Store Error Message
-$error = '';
-if (isset($_POST['submit'])) {
-    if (empty($_POST['email']) || empty($_POST['passwort'])) {
-        $error = "E-Mail Adresse oder Passwort sind fehlerhaft";
-    } else {
-        // Initializing $email and $passwort
-        $email = $_POST['email'];
-        $passwort = $_POST['passwort'];
-        // Datenbankverbindung herstellen
-        try {
-            require_once('dbconnection.php');
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $e) {
-            $error = $e->getMessage();
-        }
-        // SQL Select for all Registered Users
-        $sql = "SELECT * FROM (SELECT email,passwort from kunde UNION select email,passwort from mitarbeiter) AS U where u.passwort='$passwort' AND u.email='$email'";
-        $result = $db->query($sql);
-        $data = $result->fetch(PDO::FETCH_ASSOC);
-        if ($data) {
-            if ($data['email'] == $email && $data['passwort'] == $passwort) {
-                // Session starten
-                $_SESSION['login_user'] = $email;
-                // Zur Startseite weiterleiten
-                header("location: index.php");
-            }
-        } else {
-            $error = "E-Mail Adresse oder Passwort sind fehlerhaft";
-        }
-        $db = null;
-    }
-}
 
 
-if (isset($_SESSION['login_user'])) {
-    header("location: baeckerei.php");
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -82,82 +40,6 @@ if (isset($_SESSION['login_user'])) {
 
     <br></br>
 
-    <div id="main">
-        <h1>Register Lecker</h1>
-        <div class="container">
-            <div class="row">
-                <form method="post" class="form-horizontal col-md-20 col-md-offset-10">
-                    <div class="form-group">
-                        <label for="input1" class="col-sm-5 control-label">E-Mail Adresse</label>
-                        <div class="col-sm-10">
-                            <input type="email" name="email" required class="form-control" id="input1"
-                                   placeholder="E-Mail"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="input1" class="col-sm-5 control-label">Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="kname" required class="form-control" id="input1"
-                                   placeholder="Name"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="input1" class="col-sm-5 control-label">Geburtsdatum</label>
-                        <div class="col-sm-10">
-                            <input type="date" max="2000-01-01" name="kgeburtsdatum" required
-                                   class="form-control" id="input1" placeholder=""/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="input1" class="col-sm-5 control-label">Passwort</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="passwort" required class="form-control" id="input1"
-                                   placeholder="Passwort"/>
-                        </div>
-                    </div>
-
-                    <input type="submit" class="btn btn-primary col-md-6" value="submit" name="submit2"/>
-                </form>
-            </div>
-        </div>
-
-        <?php
-        /*
-         Quellen:
-         http://codingcyber.org/simple-crud-application-php-pdo-7284/
-         https://www.w3schools.com/php/php_mysql_insert.asp
-         https://www.formget.com/php-data-object/
-         */
-        if (isset($_POST["submit2"])) {
-            try {
-                require_once('dbconnection.php');
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                $sql = "INSERT INTO kunde (email, kname, kgeburtsdatum, bname, passwort, accesslevel)
-            VALUES(:email, :kname, :kgeburtsdatum, 'Lecker' , :passwort, 1)";
-
-
-                $result = $db->prepare($sql);
-                $res = $result->execute(array('email' => $_POST['email'],
-                    'kname' => $_POST['kname'],
-                    'kgeburtsdatum' => $_POST['kgeburtsdatum'],
-                    'passwort' => $_POST['passwort']
-                ));
-                if ($res) {
-                    echo "Ihre Daten wurden erfolgreich gespeichert";
-                } else {
-                    echo "Fehler aufgetreten";
-                }
-                $db = null;
-            } catch (PDOException $e) {
-                echo $e->getMessage();
-            }
-        }
-        ?>
-    </div>
 </div>
 </body>
 </html>
