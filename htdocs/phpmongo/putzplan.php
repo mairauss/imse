0fa265986f19cdb035b51bf5b580fde497edbc3a
Maximilian Behrens <maxi.behrens@hotmail.com>
2018-01-28 15:52:49 +0100

    putzplan php-files

---
+++ htdocs/phpmongo/putzplan.php
@@ -0,0 +1,141 @@
+<?php
+require 'vendor/autoload.php';
+include('session.php');
+$uri = "mongodb://team10:pass10@ds159187.mlab.com:59187/backshop";
+$client = new MongoDB\Client($uri);
+$collection = $client->backshop->users;
+$user_check = $_SESSION['login_user'];
+$logedinuser = $login_session;
+$cursor = $collection->find(['email' => $user_check]);
+foreach ($cursor as $document) {
+	if (isset($logedinuser)) {
+		//Administrator Rechte
+		if ($document['accesslevel'] == 9 || $document['accesslevel'] == 3) {
+			// echo "Access Level 9";
+		} else {
+			echo "Sie haben kein Zugriff auf diese Seite";
+			header('Location: baeckerei.php');
+		};
+	} else {
+		echo "Unzeireichende User Berechtigung";
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<title>Lecker: Putzplan</title>
+<head>
+    <link rel="stylesheet" href="index.css"/>
+</head>
+<body>
+<img src="b5.png" alt="logo" width="500" height="300">
+<br></br>
+<?php if (isset($logedinuser)): ?>
+    <?php if ($document['accesslevel'] == 9): ?>
+        <ul>
+            <li><a href="baeckerei.php">Lecker</a></li>
+            <li><a href="mitarbeiter.php">Mitarbeiter</a></li>
+            <li><a href="konditor.php">Konditor</a></li>
+            <li><a href="kuechengehilfe.php">Kuechengehilfe</a></li>
+            <li><a href="kunde.php">Kunde</a></li>
+            <li><a href="backwarenmanager.php">Backwaren Manager</a></li>
+            <li><a href="produkte.php">Produkte</a></li>
+            <li><a href="backwaren.php">Unsere Backwaren</a></li>
+            <li><a href="einkauf.php">Warenkorb</a></li>
+            <li><a href="bestand.php">Bestandteil</a></li>
+            <li><a class="active" href="putzplan.php">Putzplan</a><l/i>
+            <li><a href="session_logout.php">Logout</a></li>
+        </ul>
+    <?php endif; ?>
+    <?php if ($document['accesslevel'] == 1): ?>
+        <ul>
+            <li><a href="baeckerei.php">Lecker</a></li>
+            <li><a href="backwaren.php">Unsere Backwaren</a></li>
+            <li><a href="einkauf.php">Warenkorb</a></li>
+            <li><a href="bestand.php">Bestandteil</a></li>
+            <li><a href="session_logout.php">Logout</a></li>
+        </ul>
+    <?php endif; ?>
+    <?php if ($document['accesslevel'] == 2): ?>
+        <ul>
+            <li><a href="baeckerei.php">Lecker</a></li>
+            <li><a href="backwaren.php">Unsere Backwaren</a></li>
+            <li><a href="session_logout.php">Logout</a></li>
+        </ul>
+    <?php endif; ?>
+    <?php if ($document['accesslevel'] == 3): ?>
+        <ul>
+            <li><a href="baeckerei.php">Lecker</a></li>
+            <li><a href="konditor.php">Konditor</a></li>
+            <li><a href="backwaren.php">Unsere Backwaren</a></li>
+            <li><a href="einkauf.php">Warenkorb</a></li>
+            <li><a href="produkte.php">Produkte</a></li>
+            <li><a href="backen.php">Backen</a></li>
+            <li><a href="bestand.php">Bestandteil</a></li>
+            <li><a class="active" href="putzplan.php">Putzplan</a><l/i>
+            <li><a href="session_logout.php">Logout</a></li>
+        </ul>
+    <?php endif; ?>
+<?php endif; ?>
+
+
+<div class="undermenu">
+    <span class="caret"></span></button>
+    <ul class="nav-menu" role="menu" aria-labelledby="menu1">
+        <li><a href="putzplan_update.php">Bearbeiten</a></li>
+    </ul>
+</div>
+<br></br>
+
+<div id="wrapper">
+    <center>
+        <div>
+            <form id='searchform' action='putzplan' method='get'>
+                <a href='produkte.php'>Putzplan</a> ---
+                Suche Putzplan:
+                <         <input id='search' name='search' type='text' size='15'
+                       value='<?php if (isset($_GET['search'])) echo $_GET['search']; ?>'/>
+                <input id='submit' type='submit' class="testbutton" value='Search'/>
+            </form>
+        </div>
+        <?php
+        // check if search view of list view
+        if (isset($_GET['search'])) {
+            $sql = "SELECT * FROM putzplan WHERE personalnr like '%" . $_GET['search'] . "%'";
+        } else {
+            $sql = "SELECT * FROM putzplan";
+        }
+
+        // execute sql statement
+        $result = $db->query($sql);
+
+        ?>
+            <table style="width:70%">
+                <thead>
+                <tr>
+                    <th>Personal Nr.</th>
+                    <th>Kuechen Nr.</th>
+                    <th>Putz-Datum</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php
+                // fetch rows of the executed sql query
+                while ($r = $result->fetch(PDO::FETCH_ASSOC)) {
+                	?>
+                <tr>
+                    <td><?php echo $r['personalnr']; ?></td>
+                    <td><?php echo $r['kuecheNr']; ?></td>
+                    <td><?php echo $r['putzdatum']; ?></td>
+                </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+    </center>
+
+    <br></br>
+
+</body>
+</html></html>
---
+++ htdocs/phpmongo/putzplan_update.php
@@ -0,0 +1,113 @@
+<?php
+require 'vendor/autoload.php';
+include('session.php');
+$uri = "mongodb://team10:pass10@ds159187.mlab.com:59187/backshop";
+$client = new MongoDB\Client($uri);
+$collection = $client->backshop->putzplan;
+$user_check = $_SESSION['login_user'];
+$logedinuser = $login_session;
+$cursor = $collection->find(['email' => $user_check]);
+foreach ($cursor as $document) {
+	if (isset($logedinuser)) {
+		//Administrator Rechte
+		if ($document['accesslevel'] == 9 || $document['accesslevel'] == 3) {
+			// echo "Access Level 9";
+		} else {
+			echo "Sie haben kein Zugriff auf diese Seite";
+			header('Location: baeckerei.php');
+		};
+	} else {
+		echo "Unzeireichende User Berechtigung";
+	}
+}
+$uri = "mongodb://team10:pass10@ds159187.mlab.com:59187/backshop";
+$client = new MongoDB\Client($uri);
+$collection = $client->backshop->putzplan;
+$document = $collection->findOne(['personalnr' => $_GET['personalnr']]);
+
+if (isset($_POST) & !empty($_POST)) {
+	$id = $document['_id'];
+	$produkte = array (
+	'personalnr' => $_POST['barcode'];
+	'kuecheNr' => $_POST['kuecheNr'];
+	'putzdatum' => $_POST['putzdatum'];
+	);
+	
+
+	$collection->updateOne(
+			array('personalnr' => $_GET['personalnr']),
+			array('$set' => $putzplan)
+			);
+	
+	//redirectig to the display page. In our case, it is index.php
+	header("Location: putzplan.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<title>Lecker: Putzplan</title>
+<head>
+    <link rel="stylesheet" href="index.css"/>
+</head>
+<body>
+<img src="b5.png" alt="logo" width="500" height="300">
+<br></br>
+
+    <?php if ($data2['accesslevel'] == 9): ?>
+        <ul>
+            <li><a href="baeckerei.php">Lecker</a></li>
+            <li><a href="mitarbeiter.php">Mitarbeiter</a></li>
+            <li><a href="konditor.php">Konditor</a></li>
+            <li><a href="kuechengehilfe.php">Kuechengehilfe</a></li>
+            <li><a href="kunde.php">Kunde</a></li>
+            <li><a href="backwarenmanager.php">Backwaren Manager</a></li>
+            <li><a href="produkte.php">Produkte</a></li>
+            <li><a href="backwaren.php">Unsere Backwaren</a></li>
+            <li><a href="einkauf.php">Warenkorb</a></li>
+            <li><a href="bestand.php">Bestandteil</a></li>
+            <li><a class="active" href="putzplan.php">Putzplan</a><li>
+            <li><a href="session_logout.php">Logout</a></li>
+        </ul>
+    <?php endif; ?>
+    <?php if ($data2['accesslevel'] == 3): ?>
+        <ul>
+            <li><a href="baeckerei.php">Lecker</a></li>
+            <li><a href="konditor.php">Konditor</a></li>
+            <li><a href="backwaren.php">Unsere Backwaren</a></li>
+            <li><a href="einkauf.php">Warenkorb</a></li>
+            <li><a href="produkte.php">Produkte</a></li>
+            <li><a href="bestand.php">Bestandteil</a></li>
+            <li><a class="active" href="putzplan.php">Putzplan</a><l/i>
+            <li><a href="session_logout.php">Logout</a></li>
+        </ul>
+    <?php endif; ?>
+
+
+<div id="wrapper">
+    <center><h2>Putzplan Update</h2>
+
+        <div style="width: 500px; margin: 20px auto;">
+            <table width="100%" cellpadding="5" cellspacing="1" border="1">
+                <form action="" method="post">
+                    <input type="hidden" name="personalnr" value="<?php echo $personalnr; ?>">
+                    <tr>
+                        <td>Personal Nr</td>
+                        <td><input name="personalnr" type="integer" value="<?php echo $data['personalnr']; ?>"></td>
+                    </tr>
+                    <tr>
+                        <td>Kuechen Nr</td>
+                        <td><input name="kuecheNr" type="intger" value="<?php echo $data['kuecheNr']; ?>"></td>
+                    </tr>
+                    <tr>
+                        <td>Putz-Datum</td>
+                        <td><input name="putzdatum" type="date" value="<?php echo $data['putzdatum']; ?>"></td>
+                    </tr>
+                        <td><a href="putzplan.php">Back</a></td>
+                        <td><input name="submit_data" class="testbutton" type="submit" value="Update"></td>
+                    </tr>
+                </form>
+            </table>
+        </div>
+</body>
+</html>
